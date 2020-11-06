e822f14ddc71cac87bf4efbfa1fcd58d0270a377
William Sinn <59967116+sinnwc@users.noreply.github.com>
2020-11-06 09:41:28 -0500

    created remaining services

---
+++ src/services/courses/courses.class.js
@@ -0,0 +1,5 @@
+const { Service } = require('feathers-sequelize');
+
+exports.Courses = class Courses extends Service {
+  
+};
---
+++ src/services/slots/slots.service.js
@@ -0,0 +1,19 @@
+// Initializes the `slots` service on path `/slots`
+const { Slots } = require('./slots.class');
+const createModel = require('../../models/slots.model');
+const hooks = require('./slots.hooks');
+
+module.exports = function (app) {
+  const options = {
+    Model: createModel(app),
+    paginate: app.get('paginate')
+  };
+
+  // Initialize our service with any options it requires
+  app.use('/slots', new Slots(options, app));
+
+  // Get our initialized service so that we can register hooks
+  const service = app.service('slots');
+
+  service.hooks(hooks);
+};
